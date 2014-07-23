2fe1eb820da5723ee7004f7339f20ed095d23c2e
Jody Clements <jody@neomorphic.co.uk>
2014-07-23 13:52:50 -0400

    Added two empty .pm modules to make Inline happy.
    version 0.56 of the Inline module tried to compile all things in the lib
    directory, which caused it to fail, but if we add a .pm file for each of the
    files that doesn't have a .pm extension, the install works. This is a
    nasty hack that will not be necessary with future versions of Inline.

---
+++ lib/Bio/HMM/Logo/Fonts/LICENSE.pm
@@ -0,0 +1,8 @@
+package Bio::HMM::Logo::Fonts::LICENSE;
+
+=head1
+ This is a place holder that is required to make Inline::MakeMaker
+ version 0.56 happy.
+=cut
+
+1;
---
+++ lib/Bio/HMM/Logo/Fonts/SourceCodePro.pm
@@ -0,0 +1,8 @@
+package Bio::HMM::Logo::Fonts::SourcecodePro;
+
+=head2
+ This is a place holder that is required to make Inline::MakeMaker
+ version 0.56 happy.
+=cut
+
+1;
